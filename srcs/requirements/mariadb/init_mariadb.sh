b3c4701278589b190c656e18c8558256e7dc7545
jeada-si <jeada-si@inception.myguest.virtualbox.org>
2024-10-18 17:07:09 +0200

    wordpress OK

--- srcs/requirements/mariadb/init_mariadb.sh
+++ srcs/requirements/mariadb/init_mariadb.sh
@@ -6,4 +6,4 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PWD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO \`${DB_USER}\`@'%';"
 service mariadb stop
-exec mysqld_safe --port=3306
+exec mysqld_safe --port=3306 --bind-address=0.0.0.0
--- srcs/requirements/wordpress/init_wp.sh
+++ srcs/requirements/wordpress/init_wp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #https://make.wordpress.org/cli/handbook/how-to/how-to-install/
+DB_PWD=$(cat $DB_PWD_FILE)
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
@@ -8,6 +9,13 @@ chmod -R 755 /var/www/wordpress/
 cd /var/www/wordpress
 wp core download --allow-root
 wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PWD} --dbhost=mariadb:3306 --allow-root
+# cat wp-config.php
 wp core install --url=${DOMAIN_NAME} --title="INCEPTION" --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_MAIL} --allow-root
 wp user create ${WP_USER_NAME} ${WP_USER_MAIL} --role=${WP_USER_ROLE} --user_pass=${WP_USER_PWD} --allow-root
 
+# change listen port from unix socket to 9000
+sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+# create a directory for php-fpm
+mkdir -p /run/php
+# start php-fpm service in the foreground to keep the container running
+/usr/sbin/php-fpm7.4 -F
