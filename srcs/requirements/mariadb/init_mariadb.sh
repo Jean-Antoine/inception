ea4e1393a1fe9a330429029966f73f8c9a72ec52
jeada-si <jeada-si@inception.myguest.virtualbox.org>
2024-11-12 10:20:15 +0100

    ok

--- srcs/requirements/mariadb/init_mariadb.sh
+++ srcs/requirements/mariadb/init_mariadb.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 service mariadb start
 service mariadb status
+until mariadb -e "USE mysql;" ;
+do
+	echo "Waiting for MariaDB"
+	echo $DB_WP_USER
+	echo $DB_WP_PWD
+done
+echo "> CREATE DB/USER FOR WORDPRESS IF NEEDED"
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_WP_NAME}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS '${DB_WP_USER}'@'%' IDENTIFIED BY '${DB_WP_PWD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON ${DB_WP_NAME}.* TO \`${DB_WP_USER}\`@'%';"
+echo "< CREATE DB/USER FOR WORDPRESS IF NEEDED"
+echo "> STOPPING DB TO USE MYSQLD_SAFE"
 service mariadb stop
+echo "< STOPPING DB TO USE MYSQLD_SAFE"
 exec mysqld_safe --port=3306 --bind-address=0.0.0.0
